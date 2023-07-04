7e214da038fcfd332b13ebc4320b9ab7dd67b207
kunal <kunal23kmr@gmail.com>
2023-07-04 16:46:22 +0530

    adding-member

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import './App.css';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PostList from './Components/PostList';
 import Home from './Pages/Home';
 import Speaker from './Pages/Speaker';
 import Committee from './Pages/Committee';
 import Contact from './Pages/Contact';
 import Error from './Pages/Error';
 import NavBar from './Components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-const p = "Pixel_perfect_project"
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const p = "Pixel_perfect_project"
 function App() {
   return (
-    <div className='container'>
+    <div className="ck">
       <BrowserRouter>
 
         <NavBar />
+        <div className='container'>
 
-        <Routes>
-          {/* home page */}
-          <Route path="/" element={<Home />} />
+          <Routes>
+            {/* home page */}
+            <Route path="/" element={<Home />} />
 
-          {/* Speaker page */}
-          <Route path="/speaker" element={<Speaker />} />
+            {/* Speaker page */}
+            <Route path="/speaker" element={<Speaker />} />
 
-          {/* Committee page */}
-          <Route path="/committee" element={<Committee />} />
+            {/* Committee page */}
+            <Route path="/committee" element={<Committee />} />
 
 
-          {/* Contact page */}
-          <Route path="/contact" element={<Contact />} />
+            {/* Contact page */}
+            <Route path="/contact" element={<Contact />} />
 
-          {/* Error page */}
-          <Route path="*" element={<Error />} />
+            {/* Error page */}
+            <Route path="*" element={<Error />} />
 
-        </Routes>
+          </Routes>
+        </div>
 
       </BrowserRouter>
     </div>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,39 +1,92 @@
 import React from 'react';
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+// import './NavBar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './NavBar.css'
 
-// const p = ""
-
-// A:\Program Code VSC\ReactJs\pixel_perfecr_project\public\logo192.png
 const NavBar = () => {
     return (
-        <nav className='nav'>
-            <div className="logo">
-                <img src="./logo.svg" alt="Logo" />
-                <span className="site-name">Pixel Site</span>
-            </div>
-            <ul className='nav-tabs'>
-                <CustomLink to="/">Home</CustomLink>
-                <CustomLink to="/speaker">Speaker</CustomLink>
-                <CustomLink to="/committee">Committee</CustomLink>
-                <CustomLink to="/contact">Contact</CustomLink>
-            </ul>
-        </nav>
+        <div className="bright">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                {/* navbar  */}
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src='./logo.svg' alt="LOGO" /> <span className='logo-txt'>Pixel Site</span>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown link
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
     );
 };
 
 export default NavBar;
 
 
-function CustomLink({ to, children, ...props }) {
-    const resolvePath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvePath.pathname, end: true })
 
-    return (
-        <li className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>
-                {children}
-            </Link>
-        </li>
-    )
-}
+// import React from 'react';
+// import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+// import './NavBar.css'
+// // import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+// // const p = ""
+
+// // A:\Program Code VSC\ReactJs\pixel_perfecr_project\public\logo192.png
+// const NavBar = () => {
+//     return (
+//         <nav className='nav'>
+//             <div className="logo">
+//                 <img src="./logo.svg" alt="Logo" />
+//                 <span className="site-name">Pixel Site</span>
+//             </div>
+//             <ul className='nav-tabs'>
+//                 <CustomLink to="/">Home</CustomLink>
+//                 <CustomLink to="/speaker">Speaker</CustomLink>
+//                 <CustomLink to="/committee">Committee</CustomLink>
+//                 <CustomLink to="/contact">Contact</CustomLink>
+//             </ul>
+//         </nav>
+//     );
+// };
+
+// export default NavBar;
+
+
+// function CustomLink({ to, children, ...props }) {
+//     const resolvePath = useResolvedPath(to)
+//     const isActive = useMatch({ path: resolvePath.pathname, end: true })
+
+//     return (
+//         <li className={isActive ? "active" : ""}>
+//             <Link to={to} {...props}>
+//                 {children}
+//             </Link>
+//         </li>
+//     )
+// }
